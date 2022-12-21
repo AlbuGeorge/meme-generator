db51becc3735603d6f937057a51269341e832704
George Albu <george.albu1@gmail.com>
2022-12-21 19:22:31 +0200

    meme page

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,16 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import classes from './Meme.module.css'
 
 const Meme = () => {
+  const [memeUrl, setMemeUrl] = useState('')
+
+  function randomImage(arr) {
+    const memes = arr.data.memes
+    const randomMeme = Math.floor(Math.random() * memes.length)
+    return memes[randomMeme].url
+  }
+
+  useEffect(() => {
+    fetchMeme()
+  }, [])
+
+  const fetchMeme = async () => {
+    const data = await fetch('https://api.imgflip.com/get_memes')
+    const res = await data.json()
+    setMemeUrl(randomImage(res))
+  }
+
   return (
-    <form className={classes.form}>
-      <div>
-        <input type="text" name="headText" id="headText" />
-        <input type="text" name="footerText" id="footerText" />
+    <div className={classes.main__section}>
+      <div className={classes.form}>
+        <input type="text" placeholder="Top text" />
+        <input type="text" placeholder="Bottom text" />
+        <img className={classes.meme__img} src={memeUrl} />
+        <button className={classes.form__button}>Get a new meme image 🖼</button>
       </div>
-      <input type="submit" value="Get a new meme image  🖼" />
-    </form>
+    </div>
   )
 }
 
